d77d649844a3f8431073954595dc7dfb9f829a28
Anujan Partheepan <Vauki2004@gmail.com>
2023-04-12 11:02:23 +0200

    database connectie gemaakt

---
+++ site/database.php
@@ -0,0 +1,8 @@
+// Database configuratie
+$host  = "mariadb";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "GFG";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
